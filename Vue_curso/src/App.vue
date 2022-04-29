c2d39d54dfbf52dc3f63e3f1d88ca5e083c4d7f4
Marco Gondim <marcoantonioantonio949@gmail.com>
2022-04-29 16:20:09 -0300

    adicionando lifecycle hooks

--- Vue_curso/src/App.vue
+++ Vue_curso/src/App.vue
@@ -2,14 +2,17 @@
 <div>
   <componente1/>
   <p>o dado de app é: {{ teste }}</p>
+  <LifeCycle/>
 </div> 
 </template>
 <script>
 import componente1 from './components/componente1.vue'
+import LifeCycle from './components/LifeCycle.vue'
 export default {
   name: 'App',
   components: {
-    componente1
+    componente1,
+    LifeCycle
   }, data() {
     return {
       teste: "testando"
---
+++ Vue_curso/src/components/LifeCycle.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>Meu nome é: {{ nome }}</h1>  
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'LifeCycle',
+    data() {
+        return {
+            nome: 'ainda não sei'
+        }
+    },
+    created() {
+        setTimeout(() =>{
+            this.nome = 'Matheus'
+        },1000);   
+    },
+    mounted() {
+        setTimeout(() =>{
+            this.nome = 'Marco'
+        },2000);    
+    },
+}
+</script>
