70e19e4233bb857656a704e964b6066a7400ebad
Marco Gondim <marcoantonioantonio949@gmail.com>
2022-04-29 15:59:55 -0300

    coreção de erro de instalação

---
+++ Vue_curso/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+  <componente1/>
+  <p>o dado de app é: {{ teste }}</p>
+</div> 
+</template>
+<script>
+import componente1 from './components/componente1.vue'
+export default {
+  name: 'App',
+  components: {
+    componente1
+  }, data() {
+    return {
+      teste: "testando"
+    }
+  },
+}
+</script>
---
+++ Vue_curso/src/components/componente1.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Ola vue!!!</h1>
+        <br>
+        <p>Meu nome é: {{ nome }} e trabalho como: {{ profissao }}</p>
+    </div>
+</template>
+<script>
+export default {
+    name: 'componente1',
+    data() {
+        return {
+            nome: "Marcos",
+            profissao: "programador"
+        }
+    },
+}
+</script>
