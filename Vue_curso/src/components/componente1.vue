2f51d25d8fc6d8e34667c7846014bb28beb1326f
Marco Gondim <marcoantonioantonio949@gmail.com>
2022-05-02 13:42:19 -0300

    Multiplos eventos

--- Vue_curso/src/components/componente1.vue
+++ Vue_curso/src/components/componente1.vue
@@ -3,11 +3,17 @@
         <h1>Ola vue!!!</h1>
         <br>
         <p>Meu nome é: {{ nome }} e trabalho como: {{ profissao }}</p>
+        <multiplosEventos />
     </div>
 </template>
 <script>
+import multiplosEventos from './multiplosEventos.vue'
+
 export default {
     name: 'componente1',
+    components: {
+        multiplosEventos
+    },
     data() {
         return {
             nome: "Marcos",
